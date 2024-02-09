b4df18b52d8e13efcc8158a52530e40784596940
Yackov Muliavin <yackow.muliawin2015@yandex.ru>
2024-02-09 13:56:16 +0300

    We have completed the tasks for section 13.2

--- src/product.py
+++ src/product.py
@@ -9,3 +9,19 @@ class Product:
         self.description = description
         self.price = price
         self.count_in_stock = count_in_stock
+
+    def create_product(self):
+        product = Product(self.name, self.description, self.price, self.count_in_stock)
+        return product
+
+    def get_price(self):
+        return self.price
+
+    def set_price(self, new_p):
+        if new_p <= 0:
+            print('Цена введена некорректная')
+        else:
+            self.price = new_p
+
+    def __repr__(self):
+        return f'{self.name} {self.price} {self.count_in_stock}'
--- tests/test_category.py
+++ tests/test_category.py
@@ -1,31 +1,35 @@
 import pytest
 
 from src.category import Category
+from src.product import Product
+
+product_1 = Product('Молоко', 'Из цельного молока', 140.67, 45)
 
 
 @pytest.fixture()
 def category_milk():
-    return Category('Молочные', 'Из цельного молока', ['Сгущёнка', 'Молоко', 'Сгущёнка', 'Сыр'])
+    return Category('Молочные', 'Из цельного молока')
 
 
 @pytest.fixture()
 def category_beef():
-    return Category('Мясные', 'Из говядины', ['Шейка', 'Спинка', 'Лопаточная часть', 'Вырезка рибай'])
+    return Category('Мясные', 'Из говядины')
 
 
 def test_init_milk(category_milk):
     assert category_milk.name == 'Молочные'
     assert category_milk.description == 'Из цельного молока'
-    assert category_milk.products == ['Сгущёнка', 'Молоко', 'Сгущёнка', 'Сыр']
-    assert Category.total_categories == 1
-    assert Category.total_unique_products == 3
+    assert Category.total_categories == 2
 
 
 def test_init_beef(category_beef):
     assert category_beef.name == 'Мясные'
     assert category_beef.description == 'Из говядины'
-    assert category_beef.products == ['Шейка', 'Спинка', 'Лопаточная часть', 'Вырезка рибай']
-    assert Category.total_categories == 2
-    assert Category.total_unique_products == 7
+    assert Category.total_categories == 3
+
+
+def test_add_products(category_milk):
+    assert category_milk.add_products(product_1) == [product_1]
+
 
 
