ade1bcaef6bb433ee66ff9645d3544d76f9efe88
Yackov Muliavin <yackow.muliawin2015@yandex.ru>
2024-02-19 17:21:51 +0300

    15.1 Inheritance

--- tests/test_category.py
+++ tests/test_category.py
@@ -1,7 +1,7 @@
 import pytest
 
 from src.category import Category
-from src.product import Product
+from src.product import Product, Smartphone, LawnGrass
 
 
 @pytest.fixture()
@@ -11,12 +11,22 @@ def category_milk():
 
 @pytest.fixture()
 def product_milk():
-    return Product('Молоко', 'Из цельного молока', 140.67, 45)
+    return Product('Молоко', 'Из цельного молока', 140.67, 45, 'Молочные', 'Белое')
 
 
 @pytest.fixture()
 def product_cheese():
-    return Product('Сыр', 'Тильзитер', 120, 67)
+    return Product('Сыр', 'Тильзитер', 120, 67, 'Молочные', 'Жёлтый')
+
+
+@pytest.fixture()
+def product_smartphone():
+    return Smartphone('iPhone 13', 'Смартфон Apple', 91_000, 10, 'Smartphone', 'чёрный', 8, '13', '256GB')
+
+
+@pytest.fixture()
+def product_lawn_grass():
+    return LawnGrass('Bluegrass', 'Семена газонной травы', 5, 100, 'LawnGrass', 'зелёный', 'USA', 14)
 
 
 def test_init_milk(category_milk):
@@ -26,10 +36,20 @@ def test_init_milk(category_milk):
     assert Category.total_categories == 1
 
 
-def test_add_products_2(category_milk, product_milk, product_cheese):
+def test_add_products(category_milk, product_milk, product_cheese, product_smartphone, product_lawn_grass):
+    ex_no = ('Мясо', 'Говядина')
     category_milk.add_products(product_milk)
     category_milk.add_products(product_cheese)
-    assert len(category_milk._Category__products) == 2
+    category_milk.add_products(product_smartphone)
+    category_milk.add_products(product_lawn_grass)
+
+    assert len(category_milk._Category__products) == 4
+    assert category_milk._Category__products[0] == product_milk
+    assert category_milk._Category__products[1] == product_cheese
+    assert category_milk._Category__products[2] == product_smartphone
+    assert category_milk._Category__products[3] == product_lawn_grass
+    with pytest.raises(TypeError):
+        category_milk.add_products(ex_no)
 
 
 def test_get_products_list(category_milk, product_milk, product_cheese):
